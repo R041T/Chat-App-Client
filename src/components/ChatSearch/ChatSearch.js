c9160cf5cd53cd2512a129be882c0cb822635e3f
Rohit Mahesh <rohitmahesh@Rohits-MacBook-Pro.local>
2022-12-29 01:19:10 +0530

    added chat containers in the side nav with temporary static values

---
+++ src/components/ChatSearch/ChatSearch.js
@@ -0,0 +1,9 @@
+import './ChatSearch.css'
+
+export function ChatSearch(){
+    return (
+    <>
+        <input type='text' placeholder='Search chat' id = 'searchInput'></input>
+    </>
+    );
+}
