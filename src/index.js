015a80aae6325e6000f440d3554024fc1fd3f95c
Rohit Mahesh <rohitmahesh@Rohits-MacBook-Pro.local>
2023-01-26 23:51:38 +0530

    removed auto generated text on revert

--- src/index.js
+++ src/index.js
@@ -10,11 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
         <App />
-=======
       <App />
->>>>>>> parent of d2331b8 (updated and hide firebase API keys)
     </BrowserRouter>
   </React.StrictMode>
 );
