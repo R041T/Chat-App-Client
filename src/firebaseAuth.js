6039ebe1ca7de15fcb95eec81c2c01c1d82ffc5a
Rohit Mahesh <rohitmahesh@Rohits-MacBook-Pro.local>
2023-01-26 23:46:10 +0530

    reverting changes as it affected deployment

--- src/firebaseAuth.js
+++ src/firebaseAuth.js
@@ -9,13 +9,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCdfk9AMvzAlv_h7fz9W5bjPculVUvauCU",
+  authDomain: "chatapp-c9dd7.firebaseapp.com",
+  projectId: "chatapp-c9dd7",
+  storageBucket: "chatapp-c9dd7.appspot.com",
+  messagingSenderId: "179888812979",
+  appId: "1:179888812979:web:2c60c34efaabe36a1451c6",
+  measurementId: "G-HRKP326TEB"
 };
 
 const provider = new GoogleAuthProvider();
--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
-import store from './store/store'
-import { Provider } from 'react-redux'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+<<<<<<< HEAD
         <App />
+=======
+      <App />
+>>>>>>> parent of d2331b8 (updated and hide firebase API keys)
     </BrowserRouter>
   </React.StrictMode>
 );
