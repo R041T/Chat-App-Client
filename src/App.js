088786ab4395f53e313017bccd68e7ef65a354ec
Rohit Mahesh <rohitmahesh@Rohits-MacBook-Pro.local>
2023-05-04 01:38:07 +0530

    added privacy policy page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Route, Routes} from 'react-router-dom'
 import {Home} from './pages/Home/Home'
 import {Chat} from './pages/Chat/Chat'
+import {PrivacyPolicy} from './pages/PrivacyPolicy/PrivacyPolicy'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/chat' element={<Chat/>}/>
+    <Route path='/privacy-policy' element={<PrivacyPolicy/>}/>
   </Routes></>
     
   );
